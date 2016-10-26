5ecfdde46b15e5975c944b27c0139fcc2d9adca7
Jan Alexander Steffens (heftig) <jsteffens@make.tv>
2016-10-26 09:52:48 +0200

    rtmp2: Remove stub rtmpchunk methods

--- gst/rtmp2/rtmp/rtmpchunk.c
+++ gst/rtmp2/rtmp/rtmpchunk.c
@@ -31,19 +31,7 @@ GST_DEBUG_CATEGORY_STATIC (gst_rtmp_chunk_debug_category);
 
 /* prototypes */
 
-
-static void gst_rtmp_chunk_set_property (GObject * object,
-    guint property_id, const GValue * value, GParamSpec * pspec);
-static void gst_rtmp_chunk_get_property (GObject * object,
-    guint property_id, GValue * value, GParamSpec * pspec);
-static void gst_rtmp_chunk_dispose (GObject * object);
 static void gst_rtmp_chunk_finalize (GObject * object);
-
-
-enum
-{
-  PROP_0
-};
 
 /* class initialization */
 
@@ -56,9 +44,6 @@ gst_rtmp_chunk_class_init (GstRtmpChunkClass * klass)
 {
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
-  gobject_class->set_property = gst_rtmp_chunk_set_property;
-  gobject_class->get_property = gst_rtmp_chunk_get_property;
-  gobject_class->dispose = gst_rtmp_chunk_dispose;
   gobject_class->finalize = gst_rtmp_chunk_finalize;
 
 }
@@ -66,48 +51,6 @@ gst_rtmp_chunk_class_init (GstRtmpChunkClass * klass)
 static void
 gst_rtmp_chunk_init (GstRtmpChunk * rtmpchunk)
 {
-}
-
-void
-gst_rtmp_chunk_set_property (GObject * object, guint property_id,
-    const GValue * value, GParamSpec * pspec)
-{
-  GstRtmpChunk *rtmpchunk = GST_RTMP_CHUNK (object);
-
-  GST_DEBUG_OBJECT (rtmpchunk, "set_property");
-
-  switch (property_id) {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-      break;
-  }
-}
-
-void
-gst_rtmp_chunk_get_property (GObject * object, guint property_id,
-    GValue * value, GParamSpec * pspec)
-{
-  GstRtmpChunk *rtmpchunk = GST_RTMP_CHUNK (object);
-
-  GST_DEBUG_OBJECT (rtmpchunk, "get_property");
-
-  switch (property_id) {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-      break;
-  }
-}
-
-void
-gst_rtmp_chunk_dispose (GObject * object)
-{
-  GstRtmpChunk *rtmpchunk = GST_RTMP_CHUNK (object);
-
-  GST_TRACE_OBJECT (rtmpchunk, "dispose");
-
-  /* clean up as possible.  may be called multiple times */
-
-  G_OBJECT_CLASS (gst_rtmp_chunk_parent_class)->dispose (object);
 }
 
 void
