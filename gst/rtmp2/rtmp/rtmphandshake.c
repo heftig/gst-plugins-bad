699fee9c9525febcc2cb6ae6a0e130571f644203
Jan Alexander Steffens (heftig) <jsteffens@make.tv>
2018-04-27 19:22:17 +0200

    rtmp2: Use size constants to make handshake safer

--- gst/rtmp2/rtmp/rtmphandshake.c
+++ gst/rtmp2/rtmp/rtmphandshake.c
@@ -61,6 +61,13 @@ serialize_u32 (GByteArray * array, guint32 value)
   g_byte_array_append (array, (guint8 *) & value, sizeof value);
 }
 
+#define SIZE_P0 1
+#define SIZE_P1 1536
+#define SIZE_P2 SIZE_P1
+#define SIZE_P0P1 (SIZE_P0 + SIZE_P1)
+#define SIZE_P0P1P2 (SIZE_P0P1 + SIZE_P2)
+#define SIZE_RANDOM (SIZE_P1 - 8)
+
 typedef struct
 {
   GBytes *random_bytes;
@@ -69,10 +76,12 @@ typedef struct
 static GBytes *
 handshake_random_data (void)
 {
-  GByteArray *ba = g_byte_array_sized_new (1528);
+  G_STATIC_ASSERT (SIZE_RANDOM % 4 == 0);
+
+  GByteArray *ba = g_byte_array_sized_new (SIZE_RANDOM);
   gint i;
 
-  for (i = 0; i < 1528; i += 4) {
+  for (i = 0; i < SIZE_RANDOM; i += 4) {
     serialize_u32 (ba, g_random_int ());
   }
 
@@ -96,16 +105,16 @@ handshake_data_free (gpointer ptr)
 }
 
 static gboolean
-handshake_data_check (HandshakeData * data, const guint8 * peerrandom)
+handshake_data_check (HandshakeData * data, const guint8 * p2)
 {
   const guint8 *ourrandom = g_bytes_get_data (data->random_bytes, NULL);
-  return memcmp (ourrandom, peerrandom, 1528) == 0;
+  return memcmp (ourrandom, p2 + 8, SIZE_P2 - 8) == 0;
 }
 
 static GBytes *
 create_c0c1 (GBytes * random_bytes)
 {
-  GByteArray *ba = g_byte_array_sized_new (1 + 1536);
+  GByteArray *ba = g_byte_array_sized_new (SIZE_P0P1);
 
   /* C0 version */
   serialize_u8 (ba, 3);
@@ -120,8 +129,8 @@ create_c0c1 (GBytes * random_bytes)
   gst_rtmp_byte_array_append_bytes (ba, random_bytes);
 
   GST_DEBUG ("Sending C0+C1");
-  GST_MEMDUMP (">>> C0", ba->data, 1);
-  GST_MEMDUMP (">>> C1", ba->data + 1, 1536);
+  GST_MEMDUMP (">>> C0", ba->data, SIZE_P0);
+  GST_MEMDUMP (">>> C1", ba->data + SIZE_P0, SIZE_P1);
 
   return g_byte_array_free_to_bytes (ba);
 }
@@ -174,7 +183,7 @@ client_handshake1_done (GObject * source, GAsyncResult * result,
   }
 
   GST_DEBUG ("Sent C0+C1, waiting for S0+S1+S2");
-  gst_rtmp_input_stream_read_all_bytes_async (is, 1 + 1536 * 2,
+  gst_rtmp_input_stream_read_all_bytes_async (is, SIZE_P0P1P2,
       G_PRIORITY_DEFAULT, g_task_get_cancellable (task),
       client_handshake2_done, task);
 }
@@ -182,18 +191,20 @@ client_handshake1_done (GObject * source, GAsyncResult * result,
 static GBytes *
 create_c2 (const guint8 * s0s1s2)
 {
-  GByteArray *ba = g_byte_array_sized_new (1536);
+  G_STATIC_ASSERT (SIZE_P1 == SIZE_P2);
+
+  GByteArray *ba = g_byte_array_sized_new (SIZE_P2);
   gint64 c2time = g_get_monotonic_time ();
 
   /* Copy S1 to C2 */
-  g_byte_array_set_size (ba, 1536);
-  memcpy (ba->data, s0s1s2 + 1, 1536);
+  g_byte_array_set_size (ba, SIZE_P2);
+  memcpy (ba->data, s0s1s2 + SIZE_P0, SIZE_P1);
 
   /* C2 time2 */
   GST_WRITE_UINT32_BE (ba->data + 4, c2time / 1000);
 
   GST_DEBUG ("Sending C2");
-  GST_MEMDUMP (">>> C2", ba->data, 1536);
+  GST_MEMDUMP (">>> C2", ba->data, SIZE_P2);
 
   return g_byte_array_free_to_bytes (ba);
 }
@@ -220,21 +231,21 @@ client_handshake2_done (GObject * source, GAsyncResult * result,
   }
 
   s0s1s2 = g_bytes_get_data (res, &size);
-  if (size < 1 + 1536 * 2) {
-    GST_ERROR ("Short read (want %d have %" G_GSIZE_FORMAT ")", 1 + 1536 * 2,
+  if (size < SIZE_P0P1P2) {
+    GST_ERROR ("Short read (want %d have %" G_GSIZE_FORMAT ")", SIZE_P0P1P2,
         size);
     g_task_return_new_error (task, G_IO_ERROR, G_IO_ERROR_PARTIAL_INPUT,
-        "Short read (want %d have %" G_GSIZE_FORMAT ")", 1 + 1536 * 2, size);
+        "Short read (want %d have %" G_GSIZE_FORMAT ")", SIZE_P0P1P2, size);
     g_object_unref (task);
     goto out;
   }
 
   GST_DEBUG ("Got S0+S1+S2");
-  GST_MEMDUMP ("<<< S0", s0s1s2, 1);
-  GST_MEMDUMP ("<<< S1", s0s1s2 + 1, 1536);
-  GST_MEMDUMP ("<<< S2", s0s1s2 + 1 + 1536, 1536);
+  GST_MEMDUMP ("<<< S0", s0s1s2, SIZE_P0);
+  GST_MEMDUMP ("<<< S1", s0s1s2 + SIZE_P0, SIZE_P1);
+  GST_MEMDUMP ("<<< S2", s0s1s2 + SIZE_P0P1, SIZE_P2);
 
-  if (!handshake_data_check (data, s0s1s2 + 1 + 1536 + 8)) {
+  if (!handshake_data_check (data, s0s1s2 + SIZE_P0P1)) {
     GST_ERROR ("Handshake response data did not match");
     g_task_return_new_error (task, G_IO_ERROR, G_IO_ERROR_INVALID_DATA,
         "Handshake response data did not match");
